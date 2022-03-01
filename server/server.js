0f91b4c834104ddf3cda47c4a867032d4344326f
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-01 16:43:27 -0600

    added code

--- server/server.js
+++ server/server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const axios = require('axios');
+require('dotenv').config();
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
-/** ---------- EXPRESS ROUTES ---------- **/
+app.get('/giphy', (req, res) => {
+    axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&rating=pg`)
+        .then(response => res.send(response.data))
+        .catch(error => {
+            console.log('Error is', error);
+            res.sendStatus(500);
+        });
+})
 
+/** ---------- EXPRESS ROUTES ---------- **/
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,30 @@
 
 import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App () {
+
+  const [gifs, setGifs] = useState([]);
+
+  const handleClick = () => {
+    axios.get('/giphy')
+      .then(response => setGifs(response.data.data))
+      .catch(error => console.log(error))
+  }
 
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">APIS</h1>
           <h4><i>APIS</i></h4>
+          <button onClick={handleClick}>GIMME GIPHY</button>
         </header>
+          {
+            gifs.map((image, i) => (
+              <img key={i} src={image.images.fixed_height.url} />
+            ))
+          }
         <br/>
       </div>
     );
